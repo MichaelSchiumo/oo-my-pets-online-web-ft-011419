7155bce6b99c7683541c6796d022ed3e612d7df2
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-31 01:22:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -75,5 +75,9 @@ class Owner
   def list_pets 
     "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end  
+  
+  def reset_all
+    @@all.clear
+  end  
  
 end
